fc688c0ac54d4cd79701b56a6ae93e5e39a7e97f
Angel <angel.inkeziah14@gmail.com>
2025-06-24 16:10:04 +0530

    second commit

--- app.js
+++ app.js
@@ -5,29 +5,27 @@ let button = document.getElementById("button");
 
 button.onclick = () => {
     let n = document.getElementById("number").value;
-    let circle=document.getElementById("circle");
-    let square=document.getElementById("square");
-
-    let rectanglee=document.getElementById("rectangle");
+    let circle = document.getElementById("circle");
+    let square = document.getElementById("square");
+    let rectangle = document.getElementById("rectangle");
     let box = document.getElementById("box");
 
     let j;
-    n=Number(oldValue) + Number(n);
-    for(j=numberInShape; j<=n; j++)
-    {
+    n = Number(oldValue) + Number(n);
+    for (j = numberInShape; j <= n; j++) {
         var shape = document.createElement("div");
-        shape.innerHTML +=  numberInShape;
-        if(square.checked)
+        shape.innerHTML = numberInShape; // use = instead of +=
+        if (square.checked)
             shape.classList.add("square");
-        else if(circle.checked)
+        else if (circle.checked)
             shape.classList.add("circle");
-        else if(rectangle.checked)
+        else if (rectangle.checked)
             shape.classList.add("rectangle");
         else
-         document.write("Invalid Input");
+            document.write("Invalid Input");
         box.appendChild(shape);
-        numberInshape++;
-        oldValue = document.getElementById("box").lastElementChild.innerHTML;
-
+        numberInShape++; // fixed typo
     }
+
+    oldValue = document.getElementById("box").lastElementChild.innerHTML; // moved outside loop
 };
